e5b53e38d323026abe419ea4f092394c11160fc3
liyantao <liyantao@shiqiao.com>
2023-02-07 20:36:21 +0800

    动态规划题目

--- src/动态规划/股票买卖的最佳时机.js
+++ src/动态规划/股票买卖的最佳时机.js
@@ -9,6 +9,7 @@
   * 输出: 5
   * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
   * 注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+  * 
 */
 
 // 第一种解法 暴力法 两个for循环声明一个值保存最大值 不过得有顺序的  注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
@@ -44,21 +45,37 @@
  * @return {number}
  */
 
- const maxProfit = function (prices) {
-  if (prices.length <= 1) {
-      return 0
+//  const maxProfit = function (prices) {
+//   if (prices.length <= 1) {
+//       return 0
+//   }
+//   const dp = new Array() // 定义depTable 备忘录
+//   // 定义base
+//   dp[0] = 0
+//   let min = prices[0]
+//   for (let i = 1; i < prices.length; i++) {
+//       // 状态转移方程：前i天的最大收益 = max{前i-1天的最大收益，第i天的价格-前i-1天中的最小价格}
+//       // min表示前i-1天的最小价格
+//       // Math.max(dp[i - 1], prices[i] - min)就是我们的状态转移方程了
+//       dp[i] = Math.max(dp[i - 1], prices[i] - min)
+//       //  这里写在最下面表示前i-1天中的最小价格
+//       min = Math.min(min, prices[i])
+//   }
+//   return dp[prices.length - 1] // prices.length - 1表示前prices.length - 1天
+// }
+
+const maxProfit = function(prices) {
+  if(prices.length <= 1) {
+    return 0
   }
-  const dp = new Array() // 定义depTable 备忘录
-  // 定义base
+  const dp = new Array()
   dp[0] = 0
   let min = prices[0]
-  for (let i = 1; i < prices.length; i++) {
-      // 状态转移方程：前i天的最大收益 = max{前i-1天的最大收益，第i天的价格-前i-1天中的最小价格}
-      // min表示前i-1天的最小价格
-      // Math.max(dp[i - 1], prices[i] - min)就是我们的状态转移方程了
-      dp[i] = Math.max(dp[i - 1], prices[i] - min)
-      //  这里写在最下面表示前i-1天中的最小价格
-      min = Math.min(min, prices[i])
+  for(let i = 1; i < prices.length; i++) {
+    dp[i] = Math.max(dp[i - 1], prices[i] - min)
+    min = Math.min(min, prices[i])
   }
-  return dp[prices.length - 1] // prices.length - 1表示前prices.length - 1天
+  console.log('dp', dp)
+  return dp[prices.length - 1]
 }
+maxProfit([7,1,5,3,6,4])
